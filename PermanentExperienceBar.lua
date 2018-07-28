37a7dd1d13e0c7b99ce35299508b9b99b8cc254b
alexgurrola <alexgurrola1@gmail.com>
2018-07-28 05:11:39 -0700

    Working Hooks

--- PermanentExperienceBar.lua
+++ PermanentExperienceBar.lua
@@ -24,9 +24,27 @@ end
 --------------------------
 
 function PermanentExperienceBar:Initialize(self)
+    -- Handle Save Data
     self.saveData = ZO_SavedVars:New(self.name .. "Data", self.configVersion, nil, self.defaults)
     self:RepairSaveData(self)
+
+    -- Show Experience Bar
     self:Show(self)
+
+    -- Listen for Changes
+    --ZO_PreHookHandler(ZO_PlayerProgressBar, 'OnUpdate', PermanentExperienceBar.ExperienceBarOnUpdate)
+
+    -- Add the experience bar to the two hud displays so it always shows up
+	SCENE_MANAGER:GetScene("hud"):AddFragment(PLAYER_PROGRESS_BAR_FRAGMENT)
+	SCENE_MANAGER:GetScene("hudui"):AddFragment(PLAYER_PROGRESS_BAR_FRAGMENT)
+	SCENE_MANAGER:GetScene("hud"):AddFragment(PLAYER_PROGRESS_BAR_CURRENT_FRAGMENT)
+	SCENE_MANAGER:GetScene("hudui"):AddFragment(PLAYER_PROGRESS_BAR_CURRENT_FRAGMENT)
+
+    -- Add Interactive
+    --SCENE_MANAGER:GetScene("interact"):AddFragment(PLAYER_PROGRESS_BAR_FRAGMENT)
+	--SCENE_MANAGER:GetScene("interact"):AddFragment(PLAYER_PROGRESS_BAR_CURRENT_FRAGMENT)
+
+    -- Dismiss Initializer
     EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_ADD_ON_LOADED)
 end
 
@@ -66,8 +84,18 @@ function PermanentExperienceBar:Show(self)
     ZO_PlayerProgressBar:SetAlpha(1)
 end
 
+function PermanentExperienceBar.ExperienceBarOnUpdate()
+    PermanentExperienceBar:Show(PermanentExperienceBar)
+end
+
 ----------------------
 --  Register Events --
 ----------------------
 
 EVENT_MANAGER:RegisterForEvent(PermanentExperienceBar.name, EVENT_ADD_ON_LOADED, OnAddOnLoaded)
+
+------------------------
+--  Register Commands --
+------------------------
+
+SLASH_COMMANDS["/exp"] = PermanentExperienceBar.ExperienceBarOnUpdate
